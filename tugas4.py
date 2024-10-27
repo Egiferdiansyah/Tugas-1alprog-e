9668e047cb1d9612a07438f2b1386f50c64ac610
Egiferdiansyah <egiferdiansyah80@gmail.com>
2024-10-27 23:40:18 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,11 @@
+nama= "egi ferdiansyah"
+alamat= "DOMPU"
+nim= "24241176"
+nimpertama= 24
+nimakhir= 76
+hasil= nimpertama + nimakhir
+
+print("nama saya adalah "+ nama)
+print("saya berasal dari "+ alamat)
+print("nim saya "+nim)
+print(f"hasil penjumlahan dari {nimpertama} + {nimakhir} = {hasil}")
