b41000826d29b68b7c5fc5f885b5eaac67733415
Egiferdiansyah <egiferdiansyah80@gmail.com>
2024-11-18 17:02:03 +0800

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# menghitung luas persgi
+#nama : egi ferdiansyah
+
+print("menghitung luas persegi")
+panjang = int(input("masukkan panjang : "))
+lebar = int(input("masuk lebar : "))
+
+hasil = panjang * lebar
+print(f"hasil dari {panjang} x {lebar} = {hasil}")
